f4b8891128be7225786c1f226be6aa6ce2d9d631
ParvathyRaju116 <parvathyraju550@gmail.com>
2024-02-27 09:32:29 +0530

    Login and Registration UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Auth from './USER/Pages/Auth/Auth';
+import Home from './USER/Pages/Home/Home';
+import Header from './USER/Components/Header/Header';
+
+
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/auth' element={<Auth/>} ></Route>
+        <Route path='/' element={<Home/>}></Route>
+      </Routes>
     
     </div>
   );
