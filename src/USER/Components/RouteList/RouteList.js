3dfeba25ab1b18959748a52825ee965aec7daaf3
ParvathyRaju116 <parvathyraju550@gmail.com>
2024-03-20 16:23:12 +0530

    Project completed

--- src/USER/Components/RouteList/RouteList.js
+++ src/USER/Components/RouteList/RouteList.js
@@ -6,13 +6,18 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import StopList from "../StopList/StopList";
 import axios from "axios";
+import { Col, Modal, Row } from "react-bootstrap";
 
 function RouteList() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedRoute, setSelectedRoute] = useState(null);
   const [allRoutes, setAllRoutes] = useState([]);
   const token = localStorage.getItem("token");
 
-  const header = {
-    Authorization: `Token ${token}`,
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = (route) => {
+    setSelectedRoute(route);
+    setShowModal(true);
   };
 
   const getAllRoutes = async () => {
@@ -46,23 +51,22 @@ function RouteList() {
                   className='d-flex'
                   style={{ width: '100%', justifyContent: 'space-between' }}
                 >
-                  <div style={{ display: 'flex', width: '100%' }}>
+                  <div className="log" style={{ display: 'flex', width: '100%' }}>
                     <img
+                      onClick={() => handleShowModal(route)}
                       className="bustopLogo"
                       src="https://i.postimg.cc/DwB1WWDp/bus-station.png"
                       alt=""
                     />
                     <div className="ms-5 catDiv pt-2">{route.busroute.buscategory.category}</div>
                     <h3 style={{ textTransform: 'capitalize' }} className="ms-4">{route.busroute.route.name}</h3>
-                      <div className='ms-auto text-end me-5 pe-5' style={{textTransform:'capitalize'}}>
-                        <b>Starts From: </b> {route.busroute.route.starts_from} <br />
-                        <b>Ends At: </b> {route.busroute.route.ends_at}
-                   </div>
-                  <p className="me-5"> <b>Time </b> {route.busroute.routetime} <br /></p>
-
+                    <div className='ms-auto text-end me-5 pe-5' style={{textTransform:'capitalize'}}>
+                      <b>Starts From: </b> {route.busroute.route.starts_from} <br />
+                      <b>Ends At: </b> {route.busroute.route.ends_at}
+                    </div>
+                    <p className="me-5"> <b>Time </b> {route.busroute.routetime} <br /></p>
                   </div>
                 </AccordionSummary>
-
                 <AccordionDetails>
                   <div>
                     <StopList id={route?.busroute?.id}></StopList>
@@ -75,6 +79,27 @@ function RouteList() {
           )}
         </div>
       </div>
+      {selectedRoute && (
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Bus Details</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="BoDy">
+           <Row>
+            <Col>
+            
+                <b>Name:</b> {selectedRoute.busroute.bus.name} <br />
+                <b>Number Plate:</b> {selectedRoute.busroute.bus.Number_plate} <br />
+                <b>Bus Category:</b> {selectedRoute.busroute.buscategory.category} <br />
+            </Col>
+           <Col>
+           <img className="busIm" src={`http://127.0.0.1:8000${selectedRoute?.busroute?.bus?.image}`} alt="" />
+           </Col>
+           </Row>
+          </Modal.Body>
+        
+        </Modal>
+      )}
     </div>
   );
 }
